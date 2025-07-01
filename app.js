0a2cfaf42d28efd5e0652333691eddb79f1d6420
endryxv <verax703@gmail.com>
2025-07-01 11:15:41 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+function agregarProducto() {
+  alert("Función para agregar producto pendiente de implementación");
+}
+
+function registrarOrden() {
+  alert("Función para registrar orden pendiente de implementación");
+}
+
+function verFiados() {
+  alert("Función para ver fiados pendiente de implementación");
+}
